28655f8f1f7bd4258bdd9ceba0542d4f923c07cb
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-26 19:19:09 +0200

    Add ex01

---
+++ ex01/ft_list_push_front.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 18:55:48 by deydoux           #+#    #+#             */
+/*   Updated: 2023/07/26 19:18:05 by deydoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*elem;
+
+	elem = ft_create_elem(data);
+	elem->next = *begin_list;
+	*begin_list = elem;
+}
